c89d92007a5135cd3d87be00c853f375735f0a3c
Ammar Altahhan <ammar.tahhan@hungerstation.com>
2019-07-19 16:09:39 +0300

    Initial Commit

---
+++ JenkinsTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JenkinsTest
+//
+//  Created by Ammar Altahhan on 19/07/2019.
+//  Copyright © 2019 Ammar Altahhan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
